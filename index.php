fc51c75fdbaa7044cc1ff70d93984634e2a508a6
Vladislav <v.kirbaba@gmail.com>
2015-08-11 11:13:58 +0300

    Главная

--- index.php
+++ index.php
@@ -54,11 +54,42 @@
 					
 				<section class="we">
 					<h3>ПОЧЕМУ НАС ВЫБИРАЮТ:</h3>
-					<div class="we__small"></div>
-					<div class="we__lorge"></div>
-					<div class="we__small"></div>
+					<div class="we__small">
+						<img src="<?php bloginfo('template_directory'); ?>/img/court-1.png" alt="">
+						<p>Мы знае, что делать.</p>
+					</div>
+					<div class="we__large">
+						<img src="<?php bloginfo('template_directory'); ?>/img/aim5.png" alt="">
+						<p>Профессиональный подход -
+							взвешенное решение!</p>
+					</div>
+					<div class="we__small">
+						<img src="<?php bloginfo('template_directory'); ?>/img/coworkers1-2.png" alt="">
+						<p>Мы работаем на Ваш успех.</p>
+					</div>
 				</section>
 
+				<section class="homeRelations">
+					<div class="homeRelations__container">
+						<h2>Свяжитесь с нами</h2>
+						<div class="homeRelations__container__line"></div>
+						<form action="#">
+							<input type="text" class="relationsInput" placeholder="Ведите имя">
+							<input type="email" class="relationsInput" placeholder="Ведите e-mail">					
+							<textarea name="" class="relationsText" rows="1" placeholder="Ведите сообщение"></textarea>
+							<input type="submit" class="relationsSub" placeholder="Отправить">
+						</form>
+					</div>
+				</section>
+
+				<section class="homeContact">
+					<h2>Контакты</h2>
+					<div class="homeContact__line"></div>
+					<p>Адрес: Киев, Ул. Уличная 15, д.5</p>
+					<p>ourmail@sometype.com</p>
+					<p>+380(50) 555 55 55</p>
+				</section>
+				
 			</div>
 
 		</div>
